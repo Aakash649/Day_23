dabb5a9c6874518531ea04067a43f2deaeafee3c
Aakash Malvankar <10ajax649hamilton@gmail.com>
2020-08-04 14:28:50 +0530

    ArrayList UC

--- EmpWage.java
+++ EmpWage.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
+
 interface Alt
 {
-	public void attend(int Emp_Wage_per_Hr,int No_Days,int Work_Hrs );	
+	public void attend(String Cmp_Name,int Emp_Wage_per_Hr,int No_Days,int Work_Hrs );	
 }
 
 class Hello
@@ -15,7 +18,9 @@ class Attend1 implements Alt
 {
 	int Emp_Wage_per_Hr,day=0,Total_Hrs=0,No_Days,Work_Hrs;
 	int full_day=8,part_day=4,EmpWage,hrs,Total_Wage=0;
-	public void attend(int Emp_Wage_per_Hr,int No_Days,int Work_Hrs ) 
+	String Company,Cmp_Name;
+
+	public void attend(String Cmp_Name,int Emp_Wage_per_Hr,int No_Days,int Work_Hrs ) 
 	{
 	
 	while(Total_Hrs < Work_Hrs && day < No_Days)
@@ -45,7 +50,7 @@ class Attend1 implements Alt
 		EmpWage=Emp_Wage_per_Hr*hrs;
 		//System.out.println("Employee Wage: "+EmpWage+" Total Hrs: "+Total_Hrs+" On Day: " +day);
 		Total_Wage=Total_Wage+EmpWage;
-		
+		Company=Cmp_Name;
 	}
 	//System.out.println("Total Wage: "+Total_Wage);	
 	}
@@ -59,7 +64,8 @@ class EmpWage
         public static void main(String[] args)
         {
 
-		Attend1 at[]=new Attend1[4];
+		ArrayList<Attend1> at=new ArrayList<Attend1>();
+
                 Hello A1 = new Hello();
                 A1.Welcome();
 		Alt Reliance=new CompanyEmpWage(); 
@@ -68,19 +74,22 @@ class EmpWage
 		Alt Tata =new CompanyEmpWage();
 
 
-		Reliance.attend(20,20,6);
-		Tata.attend(10, 200, 8);
-		TCS.attend(15, 205, 6);
-		GVK.attend(35, 265, 7);
+		Reliance.attend("Reliance",20,20,6);
+		Tata.attend("Tata",10, 200, 8);
+		TCS.attend("TCS",15, 205, 6);
+		GVK.attend("GVK",35, 265, 7);
 
-		at[0]=(Attend1) Reliance;
-		at[1]=(Attend1)TCS;
-		at[2]=(Attend1)GVK;
-		at[3]=(Attend1)Tata;
+		at.add((Attend1) Reliance);
+		at.add((Attend1)TCS);
+		at.add((Attend1)GVK);
+		at.add((Attend1)Tata);
 
-		for(int i=0; i<=at.length-1; i++) 
+
+		for(int i=0;i<=at.size()-1;i++) 
 		{
-			System.out.println(" Wage: "+at[i].Total_Wage);
+			Attend1 ntr=at.get(i);
+			System.out.println("Company Name: "+ntr.Company+"|"+" Total Wage: "+ntr.Total_Wage);
 		}
+
        }
 }
